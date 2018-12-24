d4d2ac815bb619ba97e8dfae2f1e177165497a0b
Arshad Kazmi <arshadkazmi42@gmail.com>
2018-12-25 00:09:29 +0530

    Command and arguments parsing

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
+const Util = require('./lib/util');
+
 const [,, ...args] = process.argv;
 
-console.log(`Hello world ${args}`);
+const { alias, flag, command } = Util.parserInput(args);
+
+// If help flag is given as input, log details and return
+if (Util.isHelp(flag)) {
+    return Util.help();
+}
+
+if (!alias || alias.length === 0) {
+    console.error("Alias missing. You should give an alias to the command using --alias flag");
+    return;
+}
+
+if (!command || command.length === 0) {
+    console.error("Command missing!!!")
+    return;
+}
+
+console.log("Command is -->", command.join(' '));
+console.log("Alias is -->", alias);
