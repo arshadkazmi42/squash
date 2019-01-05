d684832df2a74a867d9948b11dd7c5cb9de5b8fb
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-01-05 10:51:16 +0530

    code restructure into a function

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-
-const Util = require('./lib/util');
-const Valdiations = require('./lib/validations');
-const FileOps = require('./lib/file-operations');
-
-const CONSTANTS = require('./lib/contants');
+const squash = require('./lib/squash');
 
 
 const [, , ...args] = process.argv;
-const { alias, flag, command } = Util.parserInput(args);
 
 
-// Print the logo
-Util.printLogo();
-
-
-/**
- * If some case passed in validation.
- * Break the flow
- */
-if (Valdiations.validate(alias, flag, command)) {
-  return;
-}
-
-
-/**
- * Check if directory exists
- * If directory does not exists then create new
- */
-if (!fs.existsSync(CONSTANTS.ROOT_DIR)) {
-  Util.createDirectory();
-}
-
-FileOps.createCommand(alias, command);
+// Squash the command
+squash(args);
