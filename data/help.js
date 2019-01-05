7f062fe94ee36250fe1470f19dc88a1a14aedc9c
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-01-05 11:05:33 +0530

    Code restructed, help flags moved to constants

---
+++ data/help.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    "flag": "--alias",
+    "description": "Adds the alias for the command, this will be treated as name for that command"
+  },
+  {
+    "flag": "--list",
+    "description": "Lists all the commands which are saved using squash"
+  },
+  {
+    "flag": "--help",
+    "description": "Show details of Squash"
+  }
+]
